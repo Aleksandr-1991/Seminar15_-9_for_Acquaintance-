47f0d087cc9f78903241217218c9aa734585d054
Aleks37 <lao35180972643335589007>
2024-03-07 20:34:10 +0400

    Change From 07.03.24

---
+++ Task_2_№67/Program.cs
@@ -0,0 +1,42 @@
+﻿// №67. Прога принимает на вход число и возвращает сумму его цифр.
+// 1 вар (со счётчиком действий): 
+/*
+void SumOfDigits(int number, int sum, int i) {
+    if (number != 0) {
+        sum += number % 10;
+        number /= 10;
+        i++;
+        System.Console.WriteLine($"На шаге {i} сумма равна: {sum}");
+        SumOfDigits(number, sum, i);
+    }
+}
+int Promt(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int number = Promt("Введите целое число: ");
+int sum = 0;
+int i = 0;
+SumOfDigits(number, sum, i);      //*/
+
+// 2 вар: /*
+int SumOfDigits(int number, int sum)
+{
+    if (number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+        return SumOfDigits(number, sum);
+    }
+    return sum;
+}
+int Promt(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+int number = Promt("Введите целое число: ");
+int sum = 0;
+Console.Write($"Сумма цифр в числе равна: {SumOfDigits(number, sum)}");      //*/
